9242c45ba79986642231960c16e627870690cdb4
Asyrique <asyrique@gmail.com>
2021-04-15 04:34:46 +0800

    update logging

--- main.ts
+++ main.ts
@@ -32,7 +32,7 @@ const appsRequest = await fetch(`${baseUrl}/graphql`, {
 });
 
 const logDBClient = new MongoClient();
-console.log(`Connecting to: ${Deno.env.get("LOGGING_MONGO_URI")}`);
+console.log(`Connecting to: ${Deno.env.get("LOGGING_MONGO_HOST")}`);
 // await logDBClient.connect(
 //   Deno.env.get("LOGGING_MONGO_URI") || "",
 // );
