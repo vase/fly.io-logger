42945524cd0461eb9396849c36718872834c66c9
Asyrique <asyrique@gmail.com>
2022-02-02 04:29:11 +0800

    update mongo version

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 // export { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
-export { MongoClient } from "https://deno.land/x/mongo@v0.27.0/mod.ts";
+export { MongoClient } from "https://deno.land/x/mongo@v0.29.1/mod.ts";
